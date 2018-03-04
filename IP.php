f97bbf7a781a863c681a72aa29b38c3d95e45191
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-03-04 20:29:20 +0400

    boo support

--- IP.php
+++ IP.php
@@ -1,6 +1,6 @@
 <?php
 namespace infrajs\ip;
-use infrajs\cache\Cache;
+use akiyatkin\boo\MemCache;
 use infrajs\load\Load;
 use infrajs\lang\Lang;
 
@@ -10,15 +10,17 @@ class IP {
 	{
 		if (!$ip) $ip = $_SERVER['REMOTE_ADDR'];
 		if ($ip === true) $ip = '';
-		
-
-		$data = Cache::exec(array(date('m.Y')),__FILE__, function ($ip) {
+		//echo '<pre>';
+		//debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+		//exit;
+		$data = MemCache::exec('Регион ip адресов', function ($ip) {
 			$src = 'http://freegeoip.net/json/'.$ip;
 			$data = file_get_contents($src);
-			$data = json_decode($data, true);
+			$data = json_decode($data, true); 
 			if (empty($data)) return $data;
+			$data['time'] = time();
 			return $data;
-		}, array($ip), isset($_GET['re']));
+		}, array($ip), ['akiyatkin\boo\Cache','getDurationTime'], ['last month']);
 
 
 		foreach(array('country_name','region_name','city','country_name') as $k) {
