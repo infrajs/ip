2133ca07dd9b1a6e8034498387eafb79af80ece2
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-05-14 23:45:51 +0400

    ipstack

--- IP.php
+++ IP.php
@@ -3,9 +3,14 @@ namespace infrajs\ip;
 use akiyatkin\boo\MemCache;
 use infrajs\load\Load;
 use infrajs\lang\Lang;
+use infrajs\config\Config;
 
 class IP {
 	public static $conf = array();
+	public static function lang($str = null) {
+		if (is_null($str)) return Lang::name('ip');
+		return Lang::str('ip', $str);
+	}
 	public static function get ($ip = false, $lang = 'en')
 	{
 		if (!$ip) $ip = $_SERVER['REMOTE_ADDR'];
@@ -13,8 +18,11 @@ class IP {
 		//echo '<pre>';
 		//debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 		//exit;
-		$data = MemCache::exec('Регион ip адресов', function ($ip) {
-			$src = 'http://freegeoip.net/json/'.$ip;
+		$data = MemCache::exec('Регионы ip адресов', function ($ip) {
+			$conf = Config::get('ip');
+			//$ip = '62.106.100.30';
+			$src = 'http://api.ipstack.com/'.$ip.'?access_key='.$conf['key'].'&output=json&language=en';
+			//$src = 'http://freegeoip.net/json/'.$ip;
 			$data = file_get_contents($src);
 			$data = json_decode($data, true); 
 			if (empty($data)) return $data;
@@ -22,10 +30,10 @@ class IP {
 			return $data;
 		}, array($ip), ['akiyatkin\boo\Cache','getDurationTime'], ['last month']);
 
-
-		foreach(array('country_name','region_name','city','country_name') as $k) {
+		foreach (array('country_name','region_name','city','country_name') as $k) {
 			if (!empty($data[$k])) $data[$k] = Lang::lang($lang, 'ip', $data[$k]);
 		}
+		
 		$data['lang'] = $lang;
 		return $data;
 	}
