e56b6895ac190840d6ff45e5f3972245ec27672e
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-08-18 20:25:55 +0400

    rest lang

--- IP.php
+++ IP.php
@@ -7,25 +7,25 @@ use infrajs\config\Config;
 
 class IP {
 	public static $conf = array();
-	public static function lang($str = null) {
+	public static function lang ($str = null) {
 		if (is_null($str)) return Lang::name('ip');
 		return Lang::str('ip', $str);
 	}
 	public static function get ($ip = false, $lang = 'en')
 	{
 		if (!$ip) $ip = $_SERVER['REMOTE_ADDR'];
-		if ($ip === true) $ip = '';
+		//if ($ip === true) $ip = '';
 		//echo '<pre>';
 		//debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 		//exit;
-
+		
 		$conf = Config::get('ip');
-		$data = MemCache::exec('Регионы ip адресов', function ($ip, $key) {
+		$data = MemCache::exec('Регионы ip адресов', function ($ip, $key, $lang) {
 			if (!empty($key)) {
 				//$ip = '62.106.100.30';
-				$src = 'http://api.ipstack.com/'.$ip.'?access_key='.$key.'&output=json&language=en';
-				//$src = 'http://freegeoip.net/json/'.$ip;
 
+				$src = 'http://api.ipstack.com/'.$ip.'?access_key='.$key.'&output=json&language='.$lang;
+				//$src = 'http://freegeoip.net/json/'.$ip;
 				$data = file_get_contents($src);
 				$data = json_decode($data, true); 
 			} else {
@@ -35,13 +35,16 @@ class IP {
 			if (empty($data)) return $data;
 			$data['time'] = time();
 			return $data;
-		}, array($ip,$conf['key']), ['akiyatkin\boo\Cache','getDurationTime'], ['last month']);
+		}, array($ip,$conf['key'], $lang), ['akiyatkin\boo\Cache','getDurationTime'], ['last month']);
 
+		//foreach (array('country_name','region_name','city','country_name') as $k) {
+		//	if (!empty($data[$k])) $data[$k] = Lang::lang($lang, 'ip', $data[$k]);
+		//	else $data[$k] = '';
+		//}
+		
 		foreach (array('country_name','region_name','city','country_name') as $k) {
-			if (!empty($data[$k])) $data[$k] = Lang::lang($lang, 'ip', $data[$k]);
-			else $data[$k] = '';
+			if (empty($data[$k])) $data[$k] = '';
 		}
-
 		$data['lang'] = $lang;
 		return $data;
 	}
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Access::test(true);
 
 return Rest::get( function () {
 	echo 'Пример теста <a href="/-ip/85.114.185.182">85.114.185.182</a>';	
-}, function($ip){
-	$res = IP::get($ip,'en');
+}, function($ip,$lang = 'en'){
+	$res = IP::get($ip, $lang);
 	return Ans::ans($res);
 });
